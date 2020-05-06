58df3b6137fd9b42135aeaf7aa841eed292a9d4f
Yury31 <ura3190@gmail.com>
2020-05-06 23:58:10 +0300

    css

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from accounts.forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def dashboard(request):
-    return render(request,
-                  'accounts/dashboard.html',
-                  {'section': 'dashboard'})
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 def user_login(request):
@@ -17,9 +11,7 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
+            user = authenticate(request,username=cd['username'],password=cd['password'])
         if user is not None:
             if user.is_active:
                 login(request, user)
@@ -33,15 +25,40 @@ def user_login(request):
     return render(request, 'accounts/login.html', {'form': form})
 
 
+@login_required
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html', {'section': 'dashboard'})
+
+
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
+            # Создаем нового пользователя, но пока не сохраняем в базу данных.
             new_user = user_form.save(commit=False)
+            # Задаем пользователю зашифрованный пароль.
             new_user.set_password(user_form.cleaned_data['password'])
+            # Сохраняем пользователя в базе данных.
             new_user.save()
-            return render(request, 'accounts/register_done.html', {'new_user' : new_user})
-        else:
-            user_form = UserRegistrationForm()
-        return render(request, 'accounts/register.html', {'user_form' : user_form})
+            # Создание профиля пользователя.
+            Profile.objects.create(user=new_user)
+            return render(request,
+                          'accounts/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'user_form': user_form})
 
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'accounts/edit.html', {'user_form': user_form, 'profile_form': profile_form})
--- diplom/settings.py
+++ diplom/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 
 
 ]
@@ -122,13 +123,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 LOGIN_REDIRECT_URL = 'dashboard'
+
 LOGIN_URL = 'login'
+
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
